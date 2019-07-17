3681870b4f09736d38c3b6c52094f0ff8b92d825
David Cole <david.cole@postcodelottery.co.uk>
2019-07-17 17:12:09 +0100

    create docs folder for github pages

---
+++ docs/precache-manifest.026ab54149f304904519e3fbf21aac3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c78eb3b5d882d7bd74f99b84776982d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d7b8261031934f5b03a",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8206ddf9d351031d494c",
+    "url": "/static/js/2.cf0d62a8.chunk.js"
+  },
+  {
+    "revision": "0d7b8261031934f5b03a",
+    "url": "/static/js/main.78122d2c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
